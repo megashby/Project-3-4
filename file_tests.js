e4ca96514e67e1273f549f6897998e860354e9e8
Meg Ashby <megashby@utexas.edu>
2017-11-21 23:58:36 -0600

    These are the tests, if someone could put them in a directory it's be great

---
+++ file_tests.js
@@ -0,0 +1,89 @@
+describe("uploadq1", function () {
+    // The 'it' function of Jasmine defined an individual test. The first argument is
+    // a description of the test that's appended to the module name. Because a module name
+    // is typically a noun, like the name of the function being tested, the description for
+    // an individual test is typically written in an action-data format.
+
+    it("hopefully unittests", function () {
+        // Invoke the unit being tested as necessary
+        var norm = uploadq1('q1', 'a1', 'a2', 'a3', 'r1');
+
+        // Check the results; "expect" and toEqual are Jasmine methods.
+        expect(norm).toEqual('q1'+'\n'+'a1'+'\n'+'a2'+'\n'+'a3'+'\n'+'r1'+'\n');
+    });
+});
+
+describe("uploadq1", function () {
+    // The 'it' function of Jasmine defined an individual test. The first argument is
+    // a description of the test that's appended to the module name. Because a module name
+    // is typically a noun, like the name of the function being tested, the description for
+    // an individual test is typically written in an action-data format.
+
+    it("hopefully unittests part 2", function () {
+        // Invoke the unit being tested as necessary
+        var norm = uploadq1('q6', 'a1', 'a2', 'a3', 'r1');
+
+        // Check the results; "expect" and toEqual are Jasmine methods.
+        expect(norm).toEqual('q6'+'\n'+'a1'+'\n'+'a2'+'\n'+'a3'+'\n'+'r1'+'\n');
+    });
+});
+
+describe("uploadq1", function () {
+    // The 'it' function of Jasmine defined an individual test. The first argument is
+    // a description of the test that's appended to the module name. Because a module name
+    // is typically a noun, like the name of the function being tested, the description for
+    // an individual test is typically written in an action-data format.
+
+    it("hopefully unittests part 2", function () {
+        // Invoke the unit being tested as necessary
+        var norm = uploadq1('q6', 'a1', 'a2', 'a3', 'r1');
+
+        // Check the results; "expect" and toEqual are Jasmine methods.
+        expect(norm).toEqual('q6'+'\n'+'a1'+'\n'+'a2'+'\n'+'a3'+'\n'+'r1'+'\n');
+    });
+});
+
+describe("uploadq1", function () {
+    // The 'it' function of Jasmine defined an individual test. The first argument is
+    // a description of the test that's appended to the module name. Because a module name
+    // is typically a noun, like the name of the function being tested, the description for
+    // an individual test is typically written in an action-data format.
+
+    it("hopefully unittests part 2", function () {
+        // Invoke the unit being tested as necessary
+        var norm = uploadq1('', 'a1', 'a2', 'a3', 'r1');
+
+        // Check the results; "expect" and toEqual are Jasmine methods.
+        expect(norm).toEqual(''+'\n'+''+'\n'+''+'\n'+''+'\n'+''+'\n');
+    });
+});
+
+describe("uploadq1", function () {
+    // The 'it' function of Jasmine defined an individual test. The first argument is
+    // a description of the test that's appended to the module name. Because a module name
+    // is typically a noun, like the name of the function being tested, the description for
+    // an individual test is typically written in an action-data format.
+
+    it("hopefully unittests part 2", function () {
+        // Invoke the unit being tested as necessary
+        var norm = uploadq1('', '', '', '', '');
+
+        // Check the results; "expect" and toEqual are Jasmine methods.
+        expect(norm).toEqual(''+'\n'+''+'\n'+''+'\n'+''+'\n'+''+'\n');
+    });
+});
+
+describe("uploadq1", function () {
+    // The 'it' function of Jasmine defined an individual test. The first argument is
+    // a description of the test that's appended to the module name. Because a module name
+    // is typically a noun, like the name of the function being tested, the description for
+    // an individual test is typically written in an action-data format.
+
+    it("hopefully unittests part 2", function () {
+        // Invoke the unit being tested as necessary
+        var norm = uploadq1('', 'a1', 'a2', '', 'r1');
+
+        // Check the results; "expect" and toEqual are Jasmine methods.
+        expect(norm).toEqual(''+'\n'+''+'\n'+''+'\n'+''+'\n'+''+'\n');
+    });
+});
